4f90d39542ce491fd7c3903c463e3a919e94d87d
dante <w9051303@naver.com>
2021-04-26 17:16:32 +0900

    update 5.16
    ilsan

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
-
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
-app.use(helmet());
+app.use(localsMiddleware);
 
 app.use("/", globalRouter);
 app.use("/user", userRouter);
--- routes.js
+++ routes.js
@@ -8,17 +8,17 @@ const SEARCH = "/search";
 // Users
 
 const USERS = "/users";
+const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
-const USER_DETAIL = "/:id";
 
 // Videos
 
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
+const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
-const VIDEO_DETAIL = "/:id";
 
 const routes = {
   home: HOME,
